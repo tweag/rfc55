fd867b420df339d8be9a4e602087dd96fbab085d
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-06-12 23:55:25 +0200

    Pin Nixpkgs and use filesets

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/c1064e7c1ff1ddff79ba961ebfedb7d83eff13ed.tar.gz";
+    sha256 = "0na57f7pfq0v51lc9mwnyqyxqw57wis3zhglxz3mlhqg07fkx4g9";
+  };
+  pkgs = import nixpkgs {
+    overlays = [];
+    config = {};
+  };
+  inherit (pkgs.lib) fileset;
+in
 
 with pkgs.python3.pkgs;
 
@@ -10,7 +20,14 @@ let
   ]));
 in buildPythonPackage {
   name = "inactive-maintainers";
-  src = ./.;
+  src = fileset.toSource {
+    root = ./.;
+    fileset = fileset.unions [
+      ./inactive_maintainers
+      ./setup.py
+      ./setup.cfg
+    ];
+  };
   propagatedBuildInputs = [
     PyGithub
   ];
